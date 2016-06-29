b7db3667dd8be36e8c1b6fd8d6baa0dcf9d00a44
Jake Bell <jakebrbell@gmail.com>
2016-06-29 14:27:19 -0700

    Add /users post route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
+    if (hashErr) {
+      return next(err);
+    }
+
+    knex('users')
+      .insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        hashed_password
+      })
+      .then((users) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
+});
 
 module.exports = router;
