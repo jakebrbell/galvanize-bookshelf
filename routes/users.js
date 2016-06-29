22f1107569db197115d5c666ebea5c9a42c827db
Jake Bell <jakebrbell@gmail.com>
2016-06-29 15:04:49 -0700

    Add validation to /users route

--- routes/users.js
+++ routes/users.js
@@ -6,25 +6,52 @@ const bcrypt = require('bcrypt');
 const knex = require('../knex');
 
 router.post('/users', (req, res, next) => {
-  bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
-    if (hashErr) {
-      return next(err);
-    }
+  const newUser = req.body;
 
-    knex('users')
-      .insert({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        hashed_password
+  if (!newUser.email || newUser.email.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('Email must not be blank');
+  }
+
+  if (!newUser.password || newUser.password.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('Password must not be blank');
+  }
+
+  knex('users')
+    .where('email', newUser.email)
+    .then((users) => {
+      if (users.length !== 0) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('Email already exists');
+      }
+
+      bcrypt.hash(newUser.password, 12, (hashErr, hashed_password) => {
+        if (hashErr) {
+          return next(hashErr);
+        }
+
+        knex('users')
+          .insert({
+            first_name: newUser.first_name,
+            last_name: newUser.last_name,
+            email: newUser.email,
+            hashed_password
+          })
+          .then((users) => {
+            res.sendStatus(200);
+          });
       })
-      .then((users) => {
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        next(err);
-      });
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
