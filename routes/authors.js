3be08bf520419a1dfa84c6aaebb69002e8d650ee
Jake Bell <jakebrbell@gmail.com>
2016-06-29 14:02:35 -0700

    Add /books routes

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,120 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/authors', (_req, res, next) => {
+  knex('authors')
+    .orderBy('id')
+    .then((authors) => {
+      res.send(authors);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/authors/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('authors')
+    .where('id', id)
+    .first()
+    .then((author) => {
+      if (!author) {
+        return next();
+      }
+
+      res.send(author);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/authors', (req, res, next) => {
+  knex('authors')
+    .insert(req.body, '*')
+    .then((authors) => {
+      res.send(authors[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/authors/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  // const authorChanges = req.body;
+  //
+  // if (authorChanges.portrait_url) {
+  //   author.portrait_url = authorChanges.portrait_url;
+  // }
+
+  knex('authors')
+    .update(req.body, '*')
+    .where('id', id)
+    .then((authors) => {
+      res.send(authors[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/authors/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('authors')
+    .where('id', id)
+    .first()
+    .then((author) => {
+      if (!author) {
+        return next();
+      }
+
+      return knex('authors')
+        .del()
+        .where('id', id)
+        .then(() => {
+          delete author.id;
+          res.send(author);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/authors/:id/books', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('books')
+    .where('author_id', id)
+    .orderBy('id')
+    .then((book) => {
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,147 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/books', (_req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/books/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('books')
+    .where('id', id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  const newBook = req.body;
+
+  if (!newBook.title || newBook.title.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('title must not be blank');
+  }
+
+  if (!newBook.genre || newBook.genre.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('genre must not be blank');
+  }
+
+  if (!newBook.description || newBook.description.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('description must not be blank');
+  }
+
+  if (!newBook.cover_url || newBook.cover_url.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('cover_url must not be blank');
+  }
+
+  const authorId = Number.parseInt(newBook.author_id);
+
+  if (Number.isNaN(authorId)) {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('author_id must be an integer')
+  }
+
+  knex('authors')
+    .where('id', authorId)
+    .first()
+    .then((author) => {
+      if (!author) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('author_id does not exist');
+      }
+
+      return knex('books')
+        .insert(newBook, '*')
+        .then((results) => {
+          res.send(results[0]);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('books')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
+  knex('books')
+    .where('id', id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('books')
+        .del()
+        .where('id', id)
+        .then(() => {
+          delete book.id;
+          res.send(book);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
