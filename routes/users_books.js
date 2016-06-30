c294622c83b75598e90810f4f6a991c397ad7605
Jake Bell <jakebrbell@gmail.com>
2016-06-30 16:24:22 -0700

    Add users_books routes

--- routes/users_books.js
+++ routes/users_books.js
@@ -2,7 +2,96 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+const checkAuth = function(req, res, next) {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+
+  next();
+};
+
+router.get('/users/books', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where('users_books.user_id', userId)
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = req.params.bookId;
+
+  knex('books')
+    .innerJoin('users_books', 'users_books.book_id', 'books.id')
+    .where('books.id', bookId)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  knex('users_books')
+    .insert({
+      user_id: userId,
+      book_id: bookId
+    }, '*')
+    .then((results) => {
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/users/books/:bookId', checkAuth, (req, res, next) => {
+  const userId = req.session.user.id;
+  const bookId = Number.parseInt(req.params.bookId);
+
+  knex('users_books')
+    .where({
+      'user_id': userId,
+      'book_id': bookId
+    })
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('users_books')
+        .del()
+        .where({
+          'user_id': userId,
+          'book_id': bookId
+        })
+        .then(() => {
+          delete book.id;
+          res.send(book);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
